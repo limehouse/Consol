9a973d5459e9adb562b1eb1a4d390560df850ad0
Phillip Webb <pwebb@ennov.com>
2019-12-23 17:05:24 +0000

    Additional navigator functionality

--- javascript/terminal.js
+++ javascript/terminal.js
@@ -1,11 +1,43 @@
-document.getElementById("nav0").addEventListener("click", n0);
-
-function n0()
-{
-  msg("Hold on tight, we're heading home!");
+const message = {
+  buttons : {
+    nav0 : {
+      Label : "Cancel",
+      Message : "Hold on tight, we're heading home!"
+    },
+    nav1 : {
+      Label : "One",
+      Message : "Clicked"
+    },
+    nav2 : {
+      Label : "Two",
+      Message : "Clicked"
+    },
+    nav3 : {
+      Label : "Three",
+      Message : "Clicked"
+    },
+    nav4 : {
+      Label : "Four",
+      Message : "Clicked"
+    },
+    nav5 : {
+      Label : "Five",
+      Message : "Clicked"
+    },
+    nav6 : {
+      Label : "Six",
+      Message : "Clicked"
+    }
+  },
+  getMessage : function(key)
+  {
+    return "[".concat(this.buttons[key].Label.toUpperCase(), "] ", this.buttons[key].Message);
+  }
 }
 
-function msg(output)
+function navigate(output)
 {
-  document.getElementById("console-return").innerHTML = output;
+  let msgs = message.getMessage(output.id);
+  document.getElementById("console-return").innerHTML = msgs;
+  console.log(msgs);
 }
